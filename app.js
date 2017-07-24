cde6144b74edeced02a0c6de17766950888f949e
Joseph Kim <joseph.kim.kr@gmail.com>
2017-07-24 16:41:10 -0700

    legibility improvements to table rows and added header row

--- app.js
+++ app.js
@@ -1,98 +1,120 @@
 'use strict';
 
-function Image(name) {
+function Product(name) {
   this.name = name;
   this.source = 'img/' + this.name + '.jpg';
   this.timesShown = 0;
   this.timesClicked = 0;
-  Image.all.push(this);
+  Product.all.push(this);
 }
 
-Image.totalClicks = 0;
-Image.all = [];
-Image.wasShown = [];
-Image.beingShown = [];
-Image.allNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+Product.totalClicks = 0;
+Product.all = [];
+Product.wasShown = [];
+Product.beingShown = [];
+Product.allNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 
 // Creates all the images and pushes them into the Images.all array
-for (var i = 0; i < Image.allNames.length; i++) {
-  new Image(Image.allNames[i]);
+for (var i = 0; i < Product.allNames.length; i++) {
+  new Product(Product.allNames[i]);
 }
 
-Image.imgElOne = document.getElementById('image_one');
-Image.imgElTwo = document.getElementById('image_two');
-Image.imgElThree = document.getElementById('image_three');
-Image.tableEl = document.getElementById('results');
+Product.imgElOne = document.getElementById('image_one');
+Product.imgElTwo = document.getElementById('image_two');
+Product.imgElThree = document.getElementById('image_three');
+Product.tableEl = document.getElementById('results');
 
 // Render function for the three image slots
 function renderThreeImages() {
   for (var j = 0; j < 3; j++) {
-    var randomIndex = Math.floor(Math.random() * Image.all.length);
-    while (Image.wasShown.indexOf(Image.all[randomIndex]) !== -1 || Image.beingShown.indexOf(Image.all[randomIndex]) !== -1) {
-      randomIndex = Math.floor(Math.random() * Image.all.length);
+    var randomIndex = Math.floor(Math.random() * Product.all.length);
+    while (Product.wasShown.indexOf(Product.all[randomIndex]) !== -1 || Product.beingShown.indexOf(Product.all[randomIndex]) !== -1) {
+      randomIndex = Math.floor(Math.random() * Product.all.length);
     }
-    Image.all[randomIndex].timesShown++;
-    Image.beingShown[j] = (Image.all[randomIndex]);
+    Product.all[randomIndex].timesShown++;
+    Product.beingShown[j] = (Product.all[randomIndex]);
   }
-  Image.imgElOne.src = Image.beingShown[0].source;
-  Image.imgElOne.style.width = '180px';
-  Image.imgElTwo.src = Image.beingShown[1].source;
-  Image.imgElTwo.style.width = '180px';
-  Image.imgElThree.src = Image.beingShown[2].source;
-  Image.imgElThree.style.width = '180px';
-  console.table(Image.wasShown);
-  console.table(Image.beingShown);
+  Product.imgElOne.src = Product.beingShown[0].source;
+  Product.imgElOne.style.width = '180px';
+  Product.imgElTwo.src = Product.beingShown[1].source;
+  Product.imgElTwo.style.width = '180px';
+  Product.imgElThree.src = Product.beingShown[2].source;
+  Product.imgElThree.style.width = '180px';
+  console.table(Product.wasShown);
+  console.table(Product.beingShown);
 }
 
 
 // The eventHandler function
 function eventHandler(e) {
-  if (e.target === Image.imgElOne) {
-    Image.beingShown[0].timesClicked++;
-  } else if (e.target === Image.imgElTwo) {
-    Image.beingShown[1].timesClicked++;
-  } else if (e.target === Image.imgElThree) {
-    Image.beingShown[2].timesClicked++;
+  if (e.target === Product.imgElOne) {
+    Product.beingShown[0].timesClicked++;
+  } else if (e.target === Product.imgElTwo) {
+    Product.beingShown[1].timesClicked++;
+  } else if (e.target === Product.imgElThree) {
+    Product.beingShown[2].timesClicked++;
   }
-  Image.totalClicks++;
-  if (Image.totalClicks === 25) {
-    Image.imgElOne.removeEventListener('click', eventHandler);
-    Image.imgElTwo.removeEventListener('click', eventHandler);
-    Image.imgElThree.removeEventListener('click', eventHandler);
+  Product.totalClicks++;
+  if (Product.totalClicks === 25) {
+    Product.imgElOne.removeEventListener('click', eventHandler);
+    Product.imgElTwo.removeEventListener('click', eventHandler);
+    Product.imgElThree.removeEventListener('click', eventHandler);
 
-    for (var i = 0; i < Image.all.length; i++) {
-      var trEl = document.createElement('tr');
+    // Make the header row
+    var trEl = document.createElement('tr');
+
+    var thEl = document.createElement('th');
+    thEl.textContent = 'Product Name';
+    trEl.appendChild(thEl);
+
+    thEl = document.createElement('th');
+    thEl.textContent = 'Times Chosen';
+    trEl.appendChild(thEl);
+
+    thEl = document.createElement('th');
+    thEl.textContent = 'Times Shown';
+    trEl.appendChild(thEl);
+
+    thEl = document.createElement('th');
+    thEl.textContent = 'Percentage of Times Chosen';
+    trEl.appendChild(thEl);
+
+    Product.tableEl.appendChild(trEl);
+
+    // The data rows
+    for (var i = 0; i < Product.all.length; i++) {
+      trEl = document.createElement('tr');
 
       var tdEl = document.createElement('td');
-      tdEl.textContent = Image.all[i].name;
+      tdEl.textContent = Product.all[i].name;
       trEl.appendChild(tdEl);
 
       tdEl = document.createElement('td');
-      tdEl.textContent = Image.all[i].timesShown;
+      tdEl.textContent = Product.all[i].timesClicked + ' time(s)';
       trEl.appendChild(tdEl);
 
       tdEl = document.createElement('td');
-      tdEl.textContent = Image.all[i].timesClicked;
+      tdEl.textContent = Product.all[i].timesShown + ' time(s)';
       trEl.appendChild(tdEl);
 
       tdEl = document.createElement('td');
       var percentageClicked;
-      if (Image.all[i].timesShown > 0) {
-        percentageClicked = (Image.all[i].timesClicked / Image.all[i].timesShown);
+      if (Product.all[i].timesShown > 0) {
+        percentageClicked = Math.round(((Product.all[i].timesClicked / Product.all[i].timesShown) * 100)) + ' percent';
       } else {
-        percentageClicked = 0;
+        percentageClicked = '0 percent';
       }
       tdEl.textContent = percentageClicked;
       trEl.appendChild(tdEl);
 
-      Image.tableEl.appendChild(trEl);
+      Product.tableEl.appendChild(trEl);
     }
 
     // >>>>>>>>>>>>>>>>>>>> DISPLAY THE DATA <<<<<<<<<<<<<<<<<<<<<
 
   } else {
     for (var j = 0; j < 3; j++) {
-      Image.wasShown[j] = Image.beingShown[j];
+      Product.wasShown[j] = Product.beingShown[j];
     }
     renderThreeImages();
   }
@@ -106,6 +128,6 @@ function eventHandler(e) {
 // For the initial three images
 renderThreeImages();
 
-Image.imgElOne.addEventListener('click', eventHandler);
-Image.imgElTwo.addEventListener('click', eventHandler);
-Image.imgElThree.addEventListener('click', eventHandler);
+Product.imgElOne.addEventListener('click', eventHandler);
+Product.imgElTwo.addEventListener('click', eventHandler);
+Product.imgElThree.addEventListener('click', eventHandler);
